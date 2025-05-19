86b60f25e337609c670f873e403ca4e261771ae1
error: 404 <45114712+UchihaDevan@users.noreply.github.com>
2025-05-19 20:24:49 -0300

    Update fetch_img.js

--- api/fetch_img.js
+++ api/fetch_img.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // 1) CORS (ajuste o * para seu domínio em produção)
-  res.setHeader('Access-Control-Allow-Origin', 'https://descubra-tudospy.online/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
